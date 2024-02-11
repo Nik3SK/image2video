f30ce16428a115ce93fe13ec977d7bec61c83f77
Petr Rybakov <prybakov@miem.hse.ru>
2024-02-12 00:16:48 +0300

    fix run_image2video.sh: extra empty lines, incorrect prompt_file path multi-line command (didn't work in docker smhw)

--- scripts/run_image2video.sh
+++ scripts/run_image2video.sh
@@ -1,26 +1,10 @@
 name="i2v_512_test"
-
 ckpt='checkpoints/i2v_512_v1/model.ckpt'
 config='configs/inference_i2v_512_v1.0.yaml'
-
-prompt_file="prompts/test_prompts.txt"
+prompt_file="prompts/i2v_prompts/test_prompts.txt"
 condimage_dir="inputs"
 res_dir="newResults"
 
-python3 uploadModel.py
+# python3 uploadModel.py
 
-python3 scripts/evaluation/inference.py \
---seed 123 \
---mode 'i2v' \
---ckpt_path $ckpt \
---config $config \
---savedir $res_dir \
---n_samples 1 \
---bs 1 --height 320 --width 512 \
---unconditional_guidance_scale 12.0 \
---ddim_steps 50 \
---ddim_eta 1.0 \
---prompt_file $prompt_file \
---cond_input $condimage_dir \
---fps 8
-
+python3 scripts/evaluation/inference.py --seed 123 --mode 'i2v' --ckpt_path $ckpt --config $config --savedir $res_dir --n_samples 1 --bs 1 --height 320 --width 512 --unconditional_guidance_scale 12.0 --ddim_steps 50 --ddim_eta 1.0 --prompt_file $prompt_file --cond_input $condimage_dir --fps 10
